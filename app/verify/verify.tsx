977d005dd1ffbbd2bdda5f9e595e5b903bdebf95
luantrum27 <hoangtheluan2016@gmail.com>
2024-06-09 22:35:02 +0700

    done

--- app/verify/verify.tsx
+++ app/verify/verify.tsx
@@ -1,164 +1,161 @@
 // import { v2 as cloudinary } from 'cloudinary';
-import axios from 'axios';
+import axios from "axios";
 import { CameraView, useCameraPermissions } from "expo-camera";
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from "expo-file-system";
 import { Stack, useLocalSearchParams } from "expo-router";
 import { useEffect, useRef, useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import * as Progress from "react-native-progress";
-
+import * as Progress from 'react-native-progress';
+let stt = 0;
 export default function Verify() {
   const [progress, setProgress] = useState(0);
   const [permission, requestPermission] = useCameraPermissions();
-  const [photoTaken, setPhotoTaken] = useState(false);
   const [isStart, setIsStart] = useState(false);
-  const [isStartCount, setIsStartCount] = useState(true);
+  const [isTaking, setIsTaking] = useState(false);
   const [countdown, setCountdown] = useState(-1);
-  const [photoCount, setPhotoCount] = useState(0);
-  const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
-  const cameraRef = useRef(null);   
+  const [isDone, setIsDone] = useState(false);
+  const cameraRef = useRef(null);
   const { name, email, studentID } = useLocalSearchParams();
-  
+  const [photos, setPhotos] = useState<string []>([])
+  const [cons, setCons] = useState<boolean >(false)
   useEffect(() => {
-    console.log("name:::", name);
-    console.log("email:::", email);
-    console.log("studentID:::", studentID);
-    
     requestPermission;
-  }, []);
+  }, [])
+
+  useEffect(() => {
+    if (isDone === true )
+      uploadImageOnCloud();
+    setCons(true)
+  }, [isDone]);
 
   if (!permission) {
     return <View />;
   }
   const takePicture = async () => {
-    
     if (cameraRef.current) {
-      
       const photo = await cameraRef.current.takePictureAsync();
-      setPhotoCount(prevCount => prevCount + 1);
-      setPhotoTaken(true);
 
-      try {
-        const base64 = await FileSystem.readAsStringAsync(photo.uri, { encoding: 'base64' }); // Đọc ảnh dưới dạng base64
-        uploadImageOnCloud(photo.uri)
-        // uploadImageOnCloud(`data:image/jpeg;base64,${base64}`)
-        console.log('Upload thành công!');
-      } catch (error) {
-        console.error('Lỗi upload:', error);
-      }      
-    }
+      const base64 = await FileSystem.readAsStringAsync(photo.uri, {
+        encoding: "base64",
+      }); 
 
+      setPhotos(prevPhotos => [...prevPhotos, base64]);   
+      // Cập nhật progress cho quá trình upload
+      setProgress(prevProgress => prevProgress + 0.5);
+      }
+      
   };
 
-  const uploadImageOnCloud = async (file: string) => {
-    const folderName = `${studentID}_${name}`;
-    console.log("file: ", file);
-    const formData = new FormData();
-    formData.append("image", file)
-    formData.append("folder", folderName)
+  const uploadImageOnCloud = async () => {
+    const cloudinary_folder = `${studentID}_${name}`;    
+    const result = await Promise.all(photos.map(async (photo, index) => {
+      let formData = new FormData();
 
-    const res = await axios.get("http://localhost:8000/upload")
-    console.log("res: ", file);
-    // xhr.open('POST', 'https://api.cloudinary.com/v1_1/dvaegokfq/image/upload');
-    // const xhr = new XMLHttpRequest();
-    // xhr.open('POST', 'https://api.cloudinary.com/v1_1/dvaegokfq/image/upload');
+      formData.append("data_image", photo);
+      formData.append("folder_name", cloudinary_folder);
+      try {
+        const res = await axios.post(
+          "http://192.168.100.215:8000/upload",
+          formData,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        );
+        
+        // Nếu tải lên thành công, tăng tiến độ lên
+        setProgress(prevProgress => prevProgress + 0.5);
+      } catch (error) {
+        console.error("Error uploading image:", error);
+      }
+    }))
+    // setIsStart(false)
+  };
 
+  // const startTakingPictures = async () => {
+  //   let count = 1;
+  //   while (count <= 100) {
+  //     await takePicture();
+  //     count++;
+  //     console.log(count);
+  //   }
+  //   setIsDone(true)
+  // };
+  const takePicturePromise = async () => {
+    await takePicture();
+    console.log('Picture taken');
+  };
   
-    // xhr.onload = () => {
-    //   if (xhr.status === 200) {
-    //     console.log('Upload thành công!');
-    //     setProgress((prevProgress) => {
-    //       const newProgress = (prevProgress + 1);        
-    //       return newProgress;
-    //     });
-    //     console.log('progress:::', progress);
-        
-        
-    //   } else {
-    //     console.error('Lỗi upload:', xhr.statusText);
-    //   }
-    // };
+  const startTakingPictures = async () => {
+    const totalPictures = 100;
+    const picturesPerSecond = 3;
+    const intervalTime = 1000 / picturesPerSecond;
+    setIsStart(true)
+    const picturePromises = [];
+    for (let i = 0; i < totalPictures; i++) {
+      picturePromises.push(takePicturePromise());
+      await new Promise((resolve) => setTimeout(resolve, intervalTime)); 
+    }
   
-    // const data = new FormData();
-    // data.append('file', file);
-    // data.append("upload_preset", "snvlqaav");
-    // data.append('cloud_name', 'dvaegokfq');
-    // data.append('folder', folderName);
-  
-    // xhr.send(data);
+    await Promise.all(picturePromises);
+    setIsDone(true); 
   }
 
-  const startTakingPictures = () => {
-    let count = 1
-    while(count <= 100) {
-      takePicture();
-      count++;
-    }
-  };
-
-  const startCountdown = () => {
-    setIsStartCount(true); 
-    setIsStart(true); 
-    setCountdown(3);
-  
-    const countdownInterval = setInterval(() => {
-      setCountdown((prevCountdown) => {
-        console.log("prevCountdown:::", prevCountdown)
-        if (prevCountdown <= 1) {
-          console.log("prevCountdown <= 1:::", prevCountdown <= 1)
-          clearInterval(countdownInterval);
-          // setCountdown(-1); // Ẩn bộ đếm khi kết thúc
-          setIsStartCount(false);
-          startTakingPictures()
-          return -1; // Reset countdown về 0
-        }
-        return prevCountdown - 1;
-      });
-    }, 1000);
-  };
-
   return (
-   <>
-   <Stack.Screen
+    <>
+      <Stack.Screen
         options={{ headerTransparent: true, headerTitle: "" }}
       ></Stack.Screen>
-    <View style={styles.container}>
-      <CameraView ref={cameraRef} style={styles.camera} facing="front">
-        {
-          (countdown === -1) ? <View><Text>""</Text></View> : (
+      <View style={styles.container}>
+        <CameraView ref={cameraRef} style={styles.camera} facing="front">
+          {countdown === -1 ? (
+            <View>
+              <Text>""</Text>
+            </View>
+          ) : (
             <View>
               <Text style={styles.cameraCounter}>{countdown}</Text>
             </View>
-          )
-        }
-        <View style={styles.markCamera}></View>
-        <View style={styles.background}></View>
-        {
-          isStart ? (
+          )}
+          <View style={styles.markCamera}></View>
+          <View style={styles.background}></View>
+          {isStart ? (
             <View style={styles.progressBarWrapper}>
               <Text style={{ fontWeight: "500", fontSize: 12 }}>
                 Verify Progress
                 <Text style={styles.progressNumber}> {progress}%</Text>
               </Text>
-              <Progress.Bar progress={progress / 100} width={200} color="#EF6F8B" />
+              <Progress.Bar
+                progress={progress / 100}
+                width={200}
+                color="#EF6F8B"
+              />
             </View>
           ) : (
-            <TouchableOpacity onPress={() => {startCountdown()}} style={styles.startBtn}>
-              <Text style={{ color: '#fff', }}>Bắt đầu</Text>
+            <TouchableOpacity
+              onPress={startTakingPictures}
+              style={styles.startBtn}
+            >
+              <Text style={{ color: "#fff" }}>Bắt đầu</Text>
             </TouchableOpacity>
-          )
-        }
-        <View style={{ position: "absolute", zIndex: 10, bottom: 10, paddingHorizontal: 20 }}>
-          <Text style={{ color: "#585858", textAlign: 'center' }}><Text style={{ color: "#EF6F8B" }}>Chú ý</Text>: Đặt khuôn mặt của bạn vào hình elip ở giữa màn hình để việc xác thực chính xác nhất</Text>
-        </View>
-      </CameraView>
-{/*        
-      {!photoTaken && (
-          <TouchableOpacity onPress={takePicture} style={styles.captureButton}>
-            <Text style={styles.text}>Take Picture</Text>
-          </TouchableOpacity>
-        )} */}
-    </View></>
+          )}
+          <View
+            style={{
+              position: "absolute",
+              zIndex: 10,
+              bottom: 10,
+              paddingHorizontal: 20,
+            }}
+          >
+            <Text style={{ color: "#585858", textAlign: "center" }}>
+              <Text style={{ color: "#EF6F8B" }}>Chú ý</Text>: Đặt khuôn mặt của
+              bạn vào hình elip ở giữa màn hình để việc xác thực chính xác nhất
+            </Text>
+          </View>
+        </CameraView>
+      </View>
+    </>
   );
 }
 
@@ -172,25 +169,25 @@ const styles = StyleSheet.create({
     position: "relative",
   },
   cameraCounter: {
-    position: 'absolute',
+    position: "absolute",
     fontSize: 300,
-    fontWeight: '500',
+    fontWeight: "500",
     top: 150,
     left: 120,
-    color: '#EF6F8B'
+    color: "#EF6F8B",
   },
   startBtn: {
     width: 100,
     height: 50,
-    backgroundColor: '#EF6F8B',
+    backgroundColor: "#EF6F8B",
     borderRadius: 8,
-    position: 'absolute',
+    position: "absolute",
     bottom: 70,
-    left: '50%',
-    transform: [{translateX: -50}],
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center'
+    left: "50%",
+    transform: [{ translateX: -50 }],
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
   },
   buttonContainer: {
     flex: 1,
@@ -208,7 +205,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     left: "50%",
     transform: [{ translateX: -100 }],
-    gap: 10
+    gap: 10,
   },
   text: {
     fontSize: 24,
@@ -227,23 +224,23 @@ const styles = StyleSheet.create({
     top: "50%",
     left: "50%",
     transform: [{ translateX: -175 }, { translateY: -250 }, { scaleY: 1.5 }],
-    zIndex: 10
+    zIndex: 10,
   },
   background: {
-    position: 'absolute',
+    position: "absolute",
     width: 400,
     height: 400,
     borderRadius: 400 / 2,
     bottom: -350,
     backgroundColor: "#fff",
     transform: [{ scaleX: 1.5 }, { scaleY: 1.5 }],
-    zIndex: -10
+    zIndex: -10,
   },
   captureButton: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 50,
-    alignSelf: 'center',
-    backgroundColor: '#EF6F8B',
+    alignSelf: "center",
+    backgroundColor: "#EF6F8B",
     paddingHorizontal: 20,
     paddingVertical: 10,
     borderRadius: 25,
